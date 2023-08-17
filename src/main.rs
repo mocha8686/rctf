9f7d5d69e9cff63dd42598c9fb25edd7354d6f92
huecester <jericho.paulino@yahoo.com.ph>
2023-08-17 02:32:50 -0700

    setup ssh

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Read;
+
+use anyhow::Result;
+use ssh2::Session;
+use tokio::net::TcpStream;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let tcp = TcpStream::connect(("pwnable.kr", 2222)).await?;
+    let mut session = Session::new()?;
+    session.set_tcp_stream(tcp);
+    session.handshake()?;
+    session.userauth_password("fd", "guest")?;
+
+    let mut channel = session.channel_session()?;
+    channel.exec("ls -l")?;
+    let mut s = String::new();
+    channel.read_to_string(&mut s)?;
+    println!("{}", s);
+    channel.wait_close()?;
+    println!("Exit code: {}", channel.exit_status()?);
+
+    Ok(())
 }
